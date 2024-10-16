8cce38ade42ca9d824fa832acae9c5f38c859260
SunnyWar <4521425+SunnyWar@users.noreply.github.com>
2024-10-15 20:24:37 -0700

    changed memory model

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 #![warn(clippy::pedantic)]
 
+#[global_allocator]
+static GLOBAL: mimalloc::MiMalloc = mimalloc::MiMalloc;
 extern crate memmap;
 
 mod benchmark;
