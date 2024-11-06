SunnyWar/Rockfish

commit 1ade100edb2666681e61a3cc1cfa202148758980

Changed rand function and now has unit tests

src/misc.rs
@@ -23,10 +23,11 @@ impl Prng {
         Prng(seed)
     }
 
+
     pub fn rand64(&mut self) -> u64 {
-        self.0 ^= self.0 >> 12;
-        self.0 ^= self.0 << 25;
-        self.0 ^= self.0 >> 27;
+        self.0 ^= self.0.rotate_right(12);
+        self.0 ^= self.0.rotate_left(25);
+        self.0 ^= self.0.rotate_right(27);
         u64::wrapping_mul(self.0, 2_685_821_657_736_338_717)
     }
 }
@@ -46,9 +47,25 @@ pub fn engine_info(to_uci: bool) -> String {
 
 #[cfg(test)]
 mod tests {
+    use super::*;
 
     #[test]
-    fn test_example() {
-        assert_eq!(2 + 2, 4);
+    fn test_new_prng() {
+        let seed = 123_456_789;
+        let prng = Prng::new(seed);
+        assert_eq!(prng.0, seed);
+    }
+
+    #[test]
+    fn test_rand64() {
+        let seed = 987_654_321;
+        let mut prng = Prng::new(seed);
+
+        // Perform some calls to rand64 and verify the output is not equal to the seed
+        let rand1 = prng.rand64();
+        let rand2 = prng.rand64();
+        assert_eq!(rand1, 11_400_667_279_617_234_321);
+        assert_eq!(rand2, 14_862_528_177_411_848_535);
+        assert_ne!(rand1, rand2);
     }
 }

commit fbf38bf32950a2285396e50d478c79820bd0b547

fix 1ade100edb2666681e61a3cc1cfa202148758980 which broke bench

src/misc.rs
@@ -23,9 +23,9 @@ impl Prng {
     }
 
     pub fn rand64(&mut self) -> u64 {
-        self.0 ^= self.0.rotate_right(12);
-        self.0 ^= self.0.rotate_left(25);
-        self.0 ^= self.0.rotate_right(27);
+        self.0 ^= self.0 >> 12;
+        self.0 ^= self.0 << 25;
+        self.0 ^= self.0 >> 27;
         u64::wrapping_mul(self.0, 2_685_821_657_736_338_717)
     }
 }
@@ -62,8 +62,8 @@ mod tests {
         // Perform some calls to rand64 and verify the output is not equal to the seed
         let rand1 = prng.rand64();
         let rand2 = prng.rand64();
-        assert_eq!(rand1, 11_400_667_279_617_234_321);
-        assert_eq!(rand2, 14_862_528_177_411_848_535);
+        assert_eq!(rand1, 18211065302896784785);
+        assert_eq!(rand2, 10667432569070492861);
         assert_ne!(rand1, rand2);
     }
 }